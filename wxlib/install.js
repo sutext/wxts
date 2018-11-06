7f06a9c3926daa20b42bfd74ee1e07586726d9c6
supertext <supertext@icloud.com>
2018-11-06 13:26:35 +0800

    wxlib

--- wxlib/install.js
+++ wxlib/install.js
@@ -1,11 +1,11 @@
 const fs = require('fs');
 let baseDir = process.cwd();
 let typesFolder = `${baseDir}/../@types`;
-let targetPath = `${baseDir}/../@types/wxts-types`;
+let targetPath = `${baseDir}/../@types/wxlib`;
 if (!fs.existsSync(typesFolder)) {
     fs.mkdirSync(typesFolder);
 }
 if (!fs.existsSync(targetPath)) {
     fs.mkdirSync(targetPath);
 }
-fs.copyFileSync(`${baseDir}/types.d.ts`, `${targetPath}/index.d.ts`);
+fs.copyFileSync(`${baseDir}/wxlib/types.d.ts`, `${targetPath}/index.d.ts`);
