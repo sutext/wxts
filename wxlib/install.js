0daff57ade2eb352a0af2277027665cfaa0dbf8e
supertext <supertext@icloud.com>
2019-03-14 15:06:51 +0800

    install script

--- wxlib/install.js
+++ wxlib/install.js
@@ -1,5 +1,9 @@
 const fs = require('fs');
 let baseDir = process.cwd();
+if (!fs.existsSync(`${baseDir}../../package.json`)) {
+    console.log('is not in local project dir! no need install!');
+    return
+}
 let typesFolder = `${baseDir}/../@types`;
 let targetPath = `${baseDir}/../@types/wxlib`;
 if (!fs.existsSync(typesFolder)) {
